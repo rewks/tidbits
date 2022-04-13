9bda2b051c9e6643484cf093e2099d70db6f79ad
rewks <46387812+rewks@users.noreply.github.com>
2022-04-13 09:50:03 +0100

    Update and rename getcrtsh_domains.py to crtsh_search.py

--- crtsh_search.py
+++ crtsh_search.py
@@ -3,13 +3,13 @@ import requests
 import sys
 import re
 
-url = sys.argv[1]
+url = sys.argv[1].lower()
 rData = requests.get(f'https://crt.sh/?q={url}')
 
 escaped_url = url.replace('.', '\\.')
 pattern = r'(?:(?!-)[A-Za-z0-9-*]{1,63}(?<!-)\.)+' + escaped_url
 prog = re.compile(pattern, re.MULTILINE)
-domains = prog.findall(rData.text)
+domains = prog.findall(rData.text.lower())
 
 uniqueDomains = list(dict.fromkeys(domains))
 
